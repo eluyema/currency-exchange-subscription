37ab899a1ad8f56738d13e784dce878cca577eb0
Danylo Hermanovych <elluyema@gmail.com>
2024-05-19 12:52:02 +0300

    add configuration and fastify

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,20 @@ import {
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
+import { validateEnvironmentVars } from './config/configuration';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
+  validateEnvironmentVars();
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
-    new FastifyAdapter({ logger: true }),
+    new FastifyAdapter({ logger: true, prefix: '/api' }),
   );
-  await app.listen(3000, '0.0.0.0');
+  const configService = app.get(ConfigService);
+
+  const port = configService.get('server.port');
+  const host = configService.get('server.host');
+
+  await app.listen(port, host);
 }
 bootstrap();
